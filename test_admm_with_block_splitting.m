4230fd55e77bfd46866021e46ed9f308e1115883
nicochaves <nico.chaves@yahoo.com>
2016-11-28 22:17:31 -0800

    Add sequential update to primal block-splitting experiment plots

--- test_admm_with_block_splitting.m
+++ test_admm_with_block_splitting.m
@@ -13,7 +13,7 @@ beta = 0.9;     % parameter (for augmenting lagrangian)
 precondition = true;
 seed = 0; % solver seed
 
-N = 10; % # number of problems to solve
+N = 2; % # number of problems to solve
 corr_tol = 0.01; % Tolerance for correctness
 num_blocks_range = [1, 5, 10, 15, 20]; % # of blocks to use for each splitting experiment
 rnd_permute = true; % Whether or not to randomly permute the block update order
@@ -27,23 +27,25 @@ for i_prob = 1:N
     [c, A, b, opt_val] = generate_linprog_problem(m,n,prob_seed);
     for i_num_blocks = 1:length(num_blocks_range)
         num_blocks = num_blocks_range(i_num_blocks);
-        for precond = [true, false]
-            [ov,~,~,~,eh] = lp_primal_admm_with_splitting(c, A, b, MAX_ITER, TOL, beta, ...
-                                    precond, num_blocks, rnd_permute, seed);
-            
-            if abs(ov - opt_val) > corr_tol
-                disp(['Block size: ',num2str(block_size)])
-                if precond
-                    disp('Using Preconditioning')
+        for rnd_perm = [true, false]  
+            for precond = [true, false]
+                [ov,~,~,~,eh] = lp_primal_admm_with_splitting(c, A, b, MAX_ITER, TOL, beta, ...
+                                        precond, num_blocks, rnd_perm, seed);
+
+                if abs(ov - opt_val) > corr_tol
+                    disp(['Block size: ',num2str(num_blocks)])
+                    if precond
+                        disp('Using Preconditioning')
+                    end
+                    disp(['Converged at:', num2str(length(eh))])
+                    warning('Incorrect Solution!')
+                    % store the number of steps used for convergence
+                    result{rnd_perm+1, precond+1}(i_num_blocks, i_prob) = -1;
+                else 
+                    % store the number of steps used for convergence
+                    result{rnd_perm+1, precond+1}(i_num_blocks, i_prob) = length(eh);
                 end
-                disp(['Converged at:', length(eh)])
-                warning('Incorrect Solution!')
-                % store the number of steps used for convergence
-                result{precond+1}(i_num_blocks, i_prob) = -1;
-            else 
-                % store the number of steps used for convergence
-                result{precond+1}(i_num_blocks, i_prob) = length(eh);
-            end
+            end    
         end
     end
 end
@@ -56,10 +58,10 @@ figure
 subplot(1,2,1)
 title('without pre-conditioning')
 plot_errorbar_param_conv(result(:,1),num_blocks_range, ...
-                [strcat('\beta = ',num2str(beta))], [0,10000])
+                {'Sequential', 'Rand Permute'}, [0,10000])
 
 subplot(1,2,2)
 title('with preconditioning')
 plot_errorbar_param_conv(result(:,2),num_blocks_range, ...
-                [strcat('\beta = ',num2str(beta))], [0,10000])
+                {'Sequential', 'Rand Permute'}, [0,10000])
 
