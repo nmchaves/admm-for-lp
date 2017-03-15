4dfe38e1f6d377770ae00d5b858520c85bc4df31
Nico Chaves <nico.chaves@yahoo.com>
2017-03-15 10:06:11 -0700

    run beta sensitivtiy experiments for primal and dual sdp

--- experiments/sdp/test_sdp.m
+++ experiments/sdp/test_sdp.m
@@ -10,9 +10,13 @@ prob_seed = 0;
 %% parameters
 MAX_ITER = 1e4; % max # of iterations
 TOL = 1e-6;     % Tolerance
-beta_range_low = linspace(0.1, 0.9, 5);     % parameter (for augmenting lagrangian)
+
+% set beta parameters to test (for augmenting lagrangian)
+% use a negative beta, which indicates to the solver that it must
+% auto-select beta
+beta_range_low = linspace(0.1, 0.9, 5);     
 beta_range_hi = linspace(2, 10, 5);
-beta_range = [beta_range_low beta_range_hi];
+beta_range = [beta_range_low, beta_range_hi 20, -1];
 N = 1; % # number of problems to solve
 corr_tol = 0.1; % Tolerance for correctness
 
@@ -23,7 +27,7 @@ for b_idx=1:length(beta_range)
 end
 
 
-%% Run Experiments for Various Beta Values
+%% Run Primal SDP Experiments for Various Beta Values
 
 figure(1)
 cmap = colormap(hsv);
@@ -35,20 +39,58 @@ for i=1:N
     disp(' ')
     disp(['Problem ',num2str(i)])
     
-    beta_idx = 0;
-    for beta=beta_range
-        beta_idx = beta_idx + 1;
+    for beta_idx = 1:length(beta_range)
+        beta = beta_range(beta_idx);
         disp(['beta=', num2str(beta)])
-        [ov,~,~,~,eh] = sdp_primal( c, A, b, MAX_ITER, TOL, beta, seed);
+        [ov,~,~,~,eh, beta_guess] = sdp_primal( c, A, b, MAX_ITER, TOL, beta, seed);
         disp(num2str(ov))
         
         % Plot the error history
-        semilogy(1:length(eh),eh, 'Color', cmap(1+(beta_idx-1)*color_spacing,:))
+        if beta > 0
+             semilogy(1:length(eh),eh, 'Color', cmap(1+(beta_idx-1)*color_spacing,:))
+        else
+            semilogy(1:length(eh),eh, '--', 'Color', cmap(1+(beta_idx-1)*color_spacing,:))
+            legend_obj{beta_idx} = strcat('\beta=',num2str(beta_guess, 3));
+        end
         hold on
     end
     legend(legend_obj)
     xlabel('Iteration')
     ylabel('Abs Error')
     title('SDP Primal')
+
+end
+
+%% Run Dual SDP Experiments for Various Beta Values
+
+figure(2)
+cmap = colormap(hsv);
+num_colors = size(cmap, 1);
+color_spacing = floor(num_colors / size(beta_range,2));
+
+for i=1:N
+    prob_seed = i-1;
+    disp(' ')
+    disp(['Problem ',num2str(i)])
+    
+    for beta_idx = 1:length(beta_range)
+        beta = beta_range(beta_idx);
+        disp(['beta=', num2str(beta)])
+        [ov,~,~,~,eh, beta_guess] = sdp_dual( c, A, b, MAX_ITER, TOL, beta, seed);
+        disp(num2str(ov))
+        
+        % Plot the error history
+        if beta > 0
+             semilogy(1:length(eh),eh, 'Color', cmap(1+(beta_idx-1)*color_spacing,:))
+        else
+            semilogy(1:length(eh),eh, '--', 'Color', cmap(1+(beta_idx-1)*color_spacing,:))
+            legend_obj{beta_idx} = strcat('\beta=',num2str(beta_guess, 3));
+        end
+        hold on
+    end
+    legend(legend_obj)
+    xlabel('Iteration')
+    ylabel('Abs Error')
+    title('SDP Dual')
 
 end
